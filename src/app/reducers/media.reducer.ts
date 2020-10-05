48f370e8dba2f4a02f5f902557867812594d85fa
Louis Motte <lomotte@gmail.com>
2020-10-05 22:36:07 +0200

    ++

--- src/app/reducers/media.reducer.ts
+++ src/app/reducers/media.reducer.ts
@@ -1,11 +1,12 @@
 import { createReducer, on } from '@ngrx/store';
 import { loadMedias } from '../actions/media.action';
+import { Media } from '../model/media.model';
 
-export const initialState = 0;
+export const initialState = new Array<Media>();
 
 const _mediaReducer = createReducer(
   initialState,
-  on(loadMedias, (state) => state + 1)/*,
+  on(loadMedias, (state) => state.push())/*,
   on(decrement, (state) => state - 1),
   on(reset, (state) => 0)*/
 );
@@ -14,3 +15,4 @@ export function mediaReducer(state, action) {
   return _mediaReducer(state, action);
 }
 // https://ngrx.io/guide/store
+// https://levelup.gitconnected.com/angular-ngrx-a-clean-and-clear-introduction-4ed61c89c1fc
